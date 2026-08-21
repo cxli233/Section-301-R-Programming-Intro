bdcbe13f7e2ebb18b1479ca2fb21d1fa9cb7e3cb
C. Li <47674556+cxli233@users.noreply.github.com>
2026-08-21 11:12:45 -0400

    Create S5_Example_script.R

---
+++ Scripts/S5_Example_script.R
@@ -0,0 +1,14 @@
+# Example script. 
+# Usage: list numbers from a starting value to an ending value by some increment. 
+# Date created: 2026-06-16. 
+# Author: Chenxin Li.
+# Last Modified: 2026-06-16. 
+# Input: A starting value, an ending value, and increment 
+# Output: A vector 
+# Required packages: none. 
+
+start <- 0
+end <- 100
+increment <- 1
+
+seq(from = start, to = end, by = increment)
